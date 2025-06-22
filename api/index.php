cce4761a21794d7994b5b9360245459d55e0940d
AlbertoDPM30 <albertodpm2000@gmail.com>
2025-06-22 19:25:55 -0400

    change: Nombre del enrutador

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
 
 require_once "controladores/usuarios.controlador.php"; // controlador de usuarios
 require_once "controladores/profesores.controlador.php"; // controlador de profesores
-require_once "controladores/plantilla.controlador.php";
+require_once "controladores/router.controlador.php"; // controlador del enrutador
 
 /*=============================================
    MODELOS
@@ -16,5 +16,5 @@ require_once "modelos/usuarios.modelo.php"; // controlador de usuarios
 require_once "modelos/profesores.modelo.php"; // controlador de profesores
 
 
-$plantilla = new ControladorPlantilla();
-$plantilla->ctrPlantilla();
+$router = new ControladorRouter();
+$router->ctrRouter();
